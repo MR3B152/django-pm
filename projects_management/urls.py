e9d2407923402b7bf7eebf888dda7cbf58ee3f73
MR3B152 <mr3b152@gmail.com>
2023-03-22 08:55:01 +0300

    6th commit

--- projects_management/urls.py
+++ projects_management/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('admin/', admin.site.urls),
     path('', include('projects.urls')),
-    # path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
